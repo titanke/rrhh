5903fe61d755abd02b4a837c7955ec2fc523c85d
titanke <elnatulol@gmail.com>
2024-08-07 23:46:59 -0500

    add personalized dates filter

--- resources/views/reports/month.blade.php
+++ resources/views/reports/month.blade.php
@@ -2,28 +2,36 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card shadow mb-4">
-                <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-primary">Asistencia mensual</h6>
-                </div>
-                <div class="card-body">
-                    <table class="table table-bordered table-striped" id="events-table">
-                        <thead>
-                            <tr>
-                                <th>Fecha</th>
-                                <th>Regimen</th>
-                                <th>DNI</th>
-                                <th>Apellidos y Nombres</th>
-                                <th>Marcaciones</th>
-                            </tr>
-                        </thead>
-                    </table>
-                </div>
-            </div>
+  <div class="row justify-content-center">
+    <div class="col-md-12">
+      <div class="card shadow mb-4">
+        <div class="card-header py-3 d-flex justify-content-between">
+          <h6 class="m-0 font-weight-bold text-primary">Asistencia mensual</h6>
+          <div class="form-inline">
+            <label for="startDate">Desde: </label>
+            <input type="date" class="form-control mr-2" id="startDate" name="startDate" required>
+            <label for="endDate">Hasta: </label>
+            <input type="date" class="form-control" id="endDate" name="endDate" required>
+            <input type="text" class="form-control" id="dni" name="dni" required>
+            <button type="button" class="btn btn-primary" id="filterButton">Filtrar</button>
+          </div>
         </div>
+        <div class="card-body">
+          <table class="table table-bordered table-striped" id="events-table">
+            <thead>
+              <tr>
+                <th>Fecha</th>
+                <th>Regimen</th>
+                <th>DNI</th>
+                <th>Apellidos y Nombres</th>
+                <th>Marcaciones</th>
+              </tr>
+            </thead>
+            <tbody></tbody> </table>
+        </div>
+      </div>
     </div>
+  </div>
 </div>
 @stop
 
@@ -31,27 +39,41 @@
 
 <script>
 $(function() {
-    $('#events-table').DataTable({
-        processing: true,
-        language: { "processing": '<div class="d-flex justify-content-center"><div class="spinner-border" role="status"></div></div>' },
-        serverSide: true,
-        ajax: 'getMonth',
-        columns: [
-            { data: 'fecha', name: 'fecha', orderable: false, searchable: false},
-            { data: 'regimen', name: 'regimen'},
-            { data: 'dni', name: 'dni'},
-            { data: 'name', name: 'name'},
-            { data: 'marcas', name: 'marcas', orderable: false, searchable: false}
+  const table = $('#events-table').DataTable({
+    processing: true,
+    serverSide: true,
+    ajax: {
+      url: 'getMonth',
+      data: function(d) {
+        d.dni = $('#dni').val();
+        d.startDate = $('#startDate').val();
+        d.endDate = $('#endDate').val();
+      }
+    },
+    columns: [
+      { data: 'fecha', name: 'fecha', orderable: false, searchable: false },
+      { data: 'regimen', name: 'regimen' },
+      { data: 'dni', name: 'dni' },
+      { data: 'name', name: 'name' },
+      { data: 'marcas', name: 'marcas', orderable: false, searchable: false }
+    ],
+    order: [[1, 'asc']],
+    columnDefs: [{ targets: 1, className: 'dt-right' }],
+            layout: {
+        top2Start: {
+            buttons: ['excel', {
+                    extend: 'pdfHtml5',
+                    orientation: 'landscape',
+                    pageSize: 'LEGAL'
+                }]
+            }
+        }
+  });
 
-        ],
-        order: [[1, 'asc']],
-        columnDefs: [{
-            targets: 1,
-            className: 'dt-right'
-        }]
-    });
+  $('#filterButton').click(function() {
+    table.ajax.reload(); 
+  });
 });
 </script>
-
 
 @endpush
